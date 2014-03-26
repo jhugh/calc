56954140f8f53e7b1fd9b52a26d42d44a14282bd
jhugh <jhugh@github.com>
2014-03-26 20:02:13 +0000

    incremental commit

--- js/calculator.js
+++ js/calculator.js
@@ -6,56 +6,52 @@ var Calculator = (function() {
        // addition operation
        addNumber: function(operand,num) {
             var op = parseInt(operand);
-            if (isNaN(op) == true) {
-                throw new Error ("Not a number");
-            }
-            if (op > 2000000000 || op < -2000000000) {
-                throw new Error ("Out of Range");
-            }            
-            return op + num;
+                if (isNaN(op) == true) {
+                    throw new Error ("Not a number");
+                }
+                if (op > 2000000000 || op < -2000000000) {
+                    throw new Error ("Out of Range");
+                }            
+                return op + num;
        },
     
        // subtraction operation
        subtractNumber: function(operand,num) {
             var op = parseInt(operand);
-            if (isNaN(op) == true) {
-                throw new Error ("Not a number");
-            }
-            if (op > 2000000000 || op < -2000000000) {
-                throw new Error ("Out of Range");
-            }            
-            return num - op;
+                if (isNaN(op) == true) {
+                    throw new Error ("Not a number");
+                }
+                if (op > 2000000000 || op < -2000000000) {
+                    throw new Error ("Out of Range");
+                }            
+                return num - op;
        },
        
        // multiplication operation
        multiplyNumber: function(operand,num) {
             var op = parseInt(operand);
-            if (isNaN(op) == true) {
-                throw new Error ("Not a number");
-            }
-            if (op > 2000000000 || op < -2000000000) {
-                throw new Error ("Out of Range");
-            }            
-            return op * num;
+                if (isNaN(op) == true) {
+                    throw new Error ("Not a number");
+                }
+                if (op > 2000000000 || op < -2000000000) {
+                    throw new Error ("Out of Range");
+                }            
+                return op * num;
        },
        
        // division operation
        divideNumber: function(operand,num) {
             var op = parseInt(operand);
-            try {
-            if (isNaN(op) == true) {
-                throw new Error ("Not a number");
-            }
-            if (op > 2000000000 || op < -2000000000) {
-                throw new Error ("Out of Range");
-            }            
-            if (op == 0) {
-                throw new Error ("Divide by zero Error");
-            }            
-            return num/op;
-            } catch (err) {
-                alert(err);
-            }
+                if (isNaN(op) == true) {
+                    throw new Error ("Not a number");
+                }
+                if (op > 2000000000 || op < -2000000000) {
+                    throw new Error ("Out of Range");
+                }            
+                if (op == 0) {
+                    throw new Error ("Divide by zero Error");
+                }            
+                return num/op;
        }
         
     }
